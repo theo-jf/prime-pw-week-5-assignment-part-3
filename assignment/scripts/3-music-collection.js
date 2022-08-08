d39a5ec60fcf135724edcaeb42672aa2d6dde1a8
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-08 17:59:27 -0500

    Completed the test of the 'addToCollection' function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,3 +12,10 @@ function addToCollection(title, artist, yearPublished) {
     return album;
 }
 
+console.log("Album added: ", addToCollection("Portfolio", "Grace Jones", 1977));
+console.log("Album added: ", addToCollection("Going for the One", "Yes", 1977));
+console.log("Album added: ", addToCollection("Kid A", "Radiohead", 2000));
+console.log("Album added: ", addToCollection("In Rainbows", "Radiohead", 2007));
+console.log("Album added: ", addToCollection("WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?", "Billie Eilish", 2019));
+console.log("Album added: ", addToCollection("IGOR", "Tyler, The Creator", 2019));
+console.log("Full collection: ", collection);
